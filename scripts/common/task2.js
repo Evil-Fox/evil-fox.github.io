5ca94e7ada373f53a968e4dd77540b581d6582dc
Evil_Fox <evil.sly.fox@gmail.com>
2018-01-28 13:57:08 +0300

    fixed erorr

--- scripts/common/task2.js
+++ scripts/common/task2.js
@@ -43,17 +43,31 @@ var task = [
   }
 ];
 
-var dialog = document.querySelector('dialog');
-var blockNow = document.getElementById('now_task');
-var blockClosed = document.getElementById('closed_task');
+var dialog       = document.querySelector('dialog');
+var blockNow     = document.getElementById('now_task');
+var blockClosed  = document.getElementById('closed_task');
 var blockOverdue = document.getElementById('overdue_task');
-var blockTask = document.getElementsByClassName('element_task');
+var blockTask    = document.getElementsByClassName('element_task');
+var addTask      = document.getElementById('addTask');
+var blockError   = document.getElementById('error');
 var overdueArray = [];
-var closedArray = [];
+var closedArray  = [];
+var today        = new Date();
+var checkDate    = new Date();
 console.log(task);
 
+// Выдаю ID
+function addId(){
+  for (var i = 0; i < task.length; i++) {
+    Object.defineProperty(task[i], 'task_id', {
+      writable : true,
+      value : +[i]
+    });
+  };
+};
+addId();
+
 // Добавляет новую задачу в массив и выводит в DOM
-var addTask = document.getElementById('addTask');
 addTask.addEventListener('click', function(){
   var nameTask, descriptionTask, dateTask;
   nameTask = document.getElementById('task_name').value;
@@ -65,10 +79,18 @@ addTask.addEventListener('click', function(){
     task_date        : dateTask,
     task_state       : 'now'
   }
-  task.unshift(newTask);
-  checkStatus();
-  checkClick();
-  console.log(task);
+  console.log(dateTask);
+  checkDate.setTime(Date.parse(dateTask));
+  if(dateTask.length == 0 || descriptionTask.length == 0 || nameTask.length == 0){
+      blockError.innerHTML = `<h4>Ошибка!</h4><p>Все поля обязательны для заполнения!</p>`
+  } else {
+      blockError.innerHTML = '';
+      task.unshift(newTask);
+      addId();
+      checkStatus();
+      checkClick();
+      console.log(task);
+  };
 });
 
 // Проверяет статус задачи
@@ -95,7 +117,7 @@ function showNow(taskNow){
   blockNow.innerHTML = '';
   for (var i in taskNow) {
     blockNow.innerHTML += `
-      <div class="element_task" data-state=${taskNow[i].task_state} data-task-name="${taskNow[i].task_name}" data-date="${taskNow[i].task_date}">
+      <div class="element_task" data-state=${taskNow[i].task_state} data-id="${taskNow[i].task_id}" data-date="${taskNow[i].task_date}">
         <h3> ${taskNow[i].task_name} </h3>
         <p> ${taskNow[i].task_description} </p>
         <hr/>
@@ -109,7 +131,7 @@ function showClosed(taskNow){
   blockClosed.innerHTML = '';
   for (var i in taskNow) {
     blockClosed.innerHTML += `
-      <div class="element_task" data-state=${taskNow[i].task_state} data-task-name="${taskNow[i].task_name}" data-date="${taskNow[i].task_date}">
+      <div class="element_task" data-state=${taskNow[i].task_state} data-id="${taskNow[i].task_id}" data-date="${taskNow[i].task_date}">
         <h3> ${taskNow[i].task_name} </h3>
         <p> ${taskNow[i].task_description} </p>
         <hr/>
@@ -121,7 +143,7 @@ function showOverdue(taskNow){
   blockOverdue.innerHTML = '';
   for (var i in taskNow) {
     blockOverdue.innerHTML += `
-      <div class="element_task" data-state=${taskNow[i].task_state} data-task-name="${taskNow[i].task_name}" data-date="${taskNow[i].task_date}">
+      <div class="element_task" data-state=${taskNow[i].task_state} data-id="${taskNow[i].task_id}" data-date="${taskNow[i].task_date}">
         <h3> ${taskNow[i].task_name} </h3>
         <p> ${taskNow[i].task_description} </p>
         <hr/>
@@ -140,14 +162,16 @@ function checkClick(){
         // Вытаскиваю кнопку удалить.
         if (target.getAttribute('name') == 'delTask') {
           deleteTaskNow(this);
+          checkChild();
         };
         // Вытаскиваю кнопку изменить. Вызывает диалог
         if (target.getAttribute('name') == 'editTask') {
-          openDialog(this);
+          openDialog(this.dataset.id);
         };
         // Вытаскиваю кнопку выполнить.
         if (target.getAttribute('name') == 'completedTask') {
           closedTask(this);
+          checkChild();
         };
       };
     };
@@ -158,6 +182,7 @@ function checkClick(){
         // Вытаскиваю кнопку удалить из выполненых. Вызываю удаление из узла.
         if (target.getAttribute('name') == 'delTask') {
            deleteTaskClosed(this);
+           checkChild();
         };
       };
     };
@@ -168,6 +193,7 @@ function checkClick(){
         // Вытаскиваю кнопку удалить из просроченых. Вызываю удаление из узла.
         if (target.getAttribute('name') == 'delTask') {
            deleteTaskOverdue(this);
+           checkChild();
         };
       };
     };
@@ -178,34 +204,47 @@ checkClick();
 // Удаляет узел в DOM. Вызывает удаление из массива. Вызывает новую проверку на клик.
 function deleteTaskNow(blockTask){
   blockNow.removeChild(blockTask);
-  rewrite(blockTask.dataset.taskName);
+  rewrite(blockTask.dataset.id);
   checkClick();
 };
 function deleteTaskClosed(blockTask){
   blockClosed.removeChild(blockTask);
-  rewrite(blockTask.dataset.taskName);
+  rewrite(blockTask.dataset.id);
   checkClick();
 };
 function deleteTaskOverdue(blockTask){
   blockOverdue.removeChild(blockTask);
-  rewrite(blockTask.dataset.taskName);
+  rewrite(blockTask.dataset.id);
   checkClick();
 };
+
 // Удаляет элемент из массива. Вызывает проверку на Статус. Вызывает построение DOM.
-function rewrite(taskName){
-  task = task.filter(t => t.task_name != taskName);
+function rewrite(id){
+  task = task.filter(t => t.task_id != id);
   checkStatus();
   console.log(task);
 };
 
+// Проверяет на пустоту узлов
+function checkChild(){
+  if (blockNow.childNodes.length == 1){
+    blockNow.innerHTML += `<h4>У вас нет задач.</h4>`
+  };
+  if (blockOverdue.childNodes.length == 1){
+    blockOverdue.innerHTML += `<h4>Лист пуст</h4>`
+  };
+  if (blockClosed.childNodes.length == 1){
+    blockClosed.innerHTML += `<h4>Лист пуст</h4>`
+  };
+};
+checkChild();
+
 // Проверяет срок выполнения. Если задача не получила дату при создании то она считаеться проваленой.
 function closedTask(closedBlock){
-  var dateTask = new Date();
-  dateTask.setTime(Date.parse(closedBlock.dataset.date));
-  var now = new Date();
-  if (dateTask.setHours(0,0,0,0) >= now.setHours(0,0,0,0)){
+  checkDate.setTime(Date.parse(closedBlock.dataset.date));
+  if (checkDate.setHours(0,0,0,0) >= today.setHours(0,0,0,0)){
     for (var state in task) {
-      if (task[state].task_name == closedBlock.dataset.taskName) {
+      if (task[state].task_id == closedBlock.dataset.id) {
         task[state].task_state = "closed";
         blockNow.innerHTML = '';
         checkStatus();
@@ -214,7 +253,7 @@ function closedTask(closedBlock){
     };
   } else {
     for (var state in task) {
-      if (task[state].task_name == closedBlock.dataset.taskName) {
+      if (task[state].task_id == closedBlock.dataset.id) {
         task[state].task_state = "overdue";
         blockNow.innerHTML = '';
         checkStatus();
@@ -229,42 +268,38 @@ document.querySelector('#closeDialog').onclick = function() {
   dialog.close();
 }
 // Открывает окно диалога и присвает текущие значения эементам диалога. Вызывает редактирование массива.
-function openDialog(editBlock){
+function openDialog(id){
   dialog.show();
   var editName = document.getElementById('edit_name');
   var editDescription = document.getElementById('edit_description');
   var editDate = document.getElementById('edit_date');
-  var oldName = editBlock.getElementsByTagName('h3');
-  var oldNameObj = Array.from(oldName);
-  var oldDescription = editBlock.getElementsByTagName('p');
-  var oldDescriptionObj = Array.from(oldDescription);
-  var oldDateObj = editBlock.dataset.date;
-  editName.value = oldNameObj[0].innerHTML;
-  editDescription.value = oldDescriptionObj[0].innerHTML;
-  editDate.value = oldDateObj;
-  editTask(editBlock);
+  for (var i = 0; i < task.length; i++) {
+    if( task[i].task_id == id){
+      editName.value = task[i].task_name;
+      editDescription.value = task[i].task_description;
+      editDate.value = task[i].task_date;
+      editTask(task[i].task_id);
+    }
+  }
 };
+
 // Редактирует элемент массива и обновляет массив.
-function editTask(newBlock){
+function editTask(id){
   var buttonEdit = document.getElementById('editTask');
-  buttonEdit.addEventListener('click', function(){
-    var editName = document.getElementById('edit_name').value;
-    var editDescription = document.getElementById('edit_description').value;
-    var editDate = document.getElementById('edit_date').value;
-    var newTask = {
-      task_name : editName,
-      task_description : editDescription,
-      task_date : editDate,
-      task_state: 'now'
-    };
-    for (var i in task) {
-      if (task[i].task_name == newBlock.dataset.taskName){
-        task[i] = newTask;
-        var edit = task[i];
+  buttonEdit.onclick = function(){
+  var editName = document.getElementById('edit_name').value;
+  var editDescription = document.getElementById('edit_description').value;
+  var editDate = document.getElementById('edit_date').value;
+    for (var i = 0; i < task.length; i++) {
+      if (task[i].task_id == id){
+        task[i].task_name = editName;
+        task[i].task_description = editDescription;
+        task[i].task_date = editDate;
+        console.log(task);
         dialog.close();
         checkStatus();
         checkClick();
       };
     };
-  });
+  };
 };
